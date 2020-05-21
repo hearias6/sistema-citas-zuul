24e05385256c6c2ccce84cc82b2ae2d91e3c5380
hans.arias <hearias6@misena.edu.co>
2020-05-21 14:36:30 -0500

    configuracion del zuul para el ruteo de usuario - seguridad

---
+++ src/test/java/com/citas/zuul/SistemaCitasZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citas.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCitasZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
